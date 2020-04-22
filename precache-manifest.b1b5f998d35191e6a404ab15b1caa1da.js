eba2bc618b4224730f16cbfded13ae37fcfb56e4
Evgeny Koval <evgenekoval@gmail.com>
2020-04-22 15:55:51 +0700

    Updates

---
+++ precache-manifest.b1b5f998d35191e6a404ab15b1caa1da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7909781299ef9c32de45aa71fbc656ac",
+    "url": "/pussyQuotes/index.html"
+  },
+  {
+    "revision": "1e462f3d39569484422a",
+    "url": "/pussyQuotes/static/css/2.a72c35d3.chunk.css"
+  },
+  {
+    "revision": "39f2af768d613084f18e",
+    "url": "/pussyQuotes/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1e462f3d39569484422a",
+    "url": "/pussyQuotes/static/js/2.6bbca85c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/pussyQuotes/static/js/2.6bbca85c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39f2af768d613084f18e",
+    "url": "/pussyQuotes/static/js/main.8e1bea0d.chunk.js"
+  },
+  {
+    "revision": "66d67ecf42de6a052c5b",
+    "url": "/pussyQuotes/static/js/runtime-main.b496eb30.js"
+  }
+]);
